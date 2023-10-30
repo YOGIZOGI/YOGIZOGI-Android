97bf3a04a56bebe077e755dc3e82298b5be766f5
wbham <wbham97@naver.com>
2023-10-30 17:34:47 +0900

    [Add] - 초기 세팅
    - Hilt, RoomDB plugin 및 dependency 추가
    - 프로젝트 구조 생성

---
+++ app/src/main/java/org/shop/yogizogi_android/YogizogiApplication.kt
@@ -0,0 +1,9 @@
+package org.shop.yogizogi_android
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class YogizogiApplication : Application() {
+
+}
--- app/src/main/java/org/shop/yogizogi_android/ui/MainActivity.kt
+++ app/src/main/java/org/shop/yogizogi_android/ui/MainActivity.kt
@@ -1,8 +1,11 @@
-package org.shop.yogizogi_android
+package org.shop.yogizogi_android.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import org.shop.yogizogi_android.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
