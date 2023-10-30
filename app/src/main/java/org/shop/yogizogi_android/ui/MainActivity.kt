7ed673280145be0ef0687599ca58a53748e387e8
wbham <wbham97@naver.com>
2023-10-30 19:15:26 +0900

    [Feat] - 스플래시 스크린 등록

--- app/src/main/java/org/shop/yogizogi_android/ui/MainActivity.kt
+++ app/src/main/java/org/shop/yogizogi_android/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package org.shop.yogizogi_android.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import org.shop.yogizogi_android.R
 
@@ -9,6 +10,7 @@ import org.shop.yogizogi_android.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
     }
 }
