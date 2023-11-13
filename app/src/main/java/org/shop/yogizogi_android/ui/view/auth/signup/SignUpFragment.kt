160074b9414b0a3509d466ff8da1b1e9557171f9
wbham <wbham97@naver.com>
2023-11-13 14:25:28 +0900

    [Feat] #9 - 회원가입 과정 설정
    - SignUpFragment안에 FragmentContainer를 만들어서 각 Process가 하나의 ViewModel을 공유하고 그 과정에 따른 step이 공유되도록 설정

--- app/src/main/java/org/shop/yogizogi_android/ui/view/auth/signup/SignUpFragment.kt
+++ app/src/main/java/org/shop/yogizogi_android/ui/view/auth/signup/SignUpFragment.kt
@@ -2,16 +2,17 @@ package org.shop.yogizogi_android.ui.view.auth.signup
 
 import dagger.hilt.android.AndroidEntryPoint
 import org.shop.yogizogi_android.R
-import org.shop.yogizogi_android.databinding.FragmentInputNumBinding
+import org.shop.yogizogi_android.databinding.FragmentSignUpBinding
 import org.shop.yogizogi_android.ui.base.BaseFragment
 
 @AndroidEntryPoint
-class InputNumFragment : BaseFragment<FragmentInputNumBinding, SignUpViewModel>(
+class SignUpFragment : BaseFragment<FragmentSignUpBinding, SignUpViewModel>(
     SignUpViewModel::class.java,
-    R.layout.fragment_input_num
+    R.layout.fragment_sign_up
 ) {
     override fun initView() {
-
+        binding.sliderSignupProcess.value = 2.0f
+        binding.sliderSignupProcess.isEnabled = false
     }
 
     override fun initAfterBinding() {
---
+++ app/src/main/java/org/shop/yogizogi_android/ui/view/auth/signup/process/PhoneFragment.kt
@@ -0,0 +1,19 @@
+package org.shop.yogizogi_android.ui.view.auth.signup.process
+
+import org.shop.yogizogi_android.R
+import org.shop.yogizogi_android.databinding.FragmentPhoneBinding
+import org.shop.yogizogi_android.ui.base.BaseFragment
+import org.shop.yogizogi_android.ui.view.auth.signup.SignUpViewModel
+
+class PhoneFragment : BaseFragment<FragmentPhoneBinding, SignUpViewModel>(
+    SignUpViewModel::class.java,
+    R.layout.fragment_phone
+) {
+    override fun initView() {
+
+    }
+
+    override fun initAfterBinding() {
+
+    }
+}
