93e32f07e8551536498fce058743289e6103b6a9
wbham <wbham97@naver.com>
2023-12-01 13:07:27 +0900

    [Add] #18 - 전체 리뷰 layout
    - 전체 리뷰 layout 구성
    - viewpager로 이미지 리스트 슬라이드 가능하도록 보여줄 예정
    - 현재 리뷰 본문을 TextView로 보여주고 있지만 이를 ReadMoreTextView로 업데이트 예정
    - 즐겨찾기와 같은 경우 동일한 크기의 이미지를 selector로 구분할 예정
    - 필터 View 구성 검토해야함

---
+++ app/src/main/java/org/shop/yogizogi_android/ui/view/main/home/storereviews/StoreReviewFragment.kt
@@ -0,0 +1,19 @@
+package org.shop.yogizogi_android.ui.view.main.home.storereviews
+
+import org.shop.yogizogi_android.R
+import org.shop.yogizogi_android.databinding.FragmentStoreReviewBinding
+import org.shop.yogizogi_android.ui.base.BaseFragment
+import org.shop.yogizogi_android.ui.view.main.home.HomeViewModel
+
+class StoreReviewFragment : BaseFragment<FragmentStoreReviewBinding, HomeViewModel>(
+    HomeViewModel::class.java,
+    R.layout.fragment_store_review
+) {
+    override fun initView() {
+
+    }
+
+    override fun initAfterBinding() {
+
+    }
+}
