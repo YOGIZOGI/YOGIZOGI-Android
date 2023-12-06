597b6e86b668cf4409c9be3e8b0ba5845ae4eaaf
wbham <wbham97@naver.com>
2023-12-06 16:38:38 +0900

    [Chore] #26 - Update setting

--- app/src/main/java/org/shop/yogizogi_android/ui/view/main/setting/SettingViewModel.kt
+++ app/src/main/java/org/shop/yogizogi_android/ui/view/main/setting/SettingViewModel.kt
@@ -1,4 +1,4 @@
-package org.shop.yogizogi_android.ui.view.main.follow
+package org.shop.yogizogi_android.ui.view.main.setting
 
 import dagger.hilt.android.lifecycle.HiltViewModel
 import org.shop.yogizogi_android.ui.base.BaseViewModel
